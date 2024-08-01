22406dc1e76a88cea7e0c35ce88842934a981c48
Vamshi <krishnavamshi6199@gmail.com>
2024-08-01 18:09:40 +0530

    P2C&C2P

--- force-app/main/default/lwc/parentToChild/parentToChild.js
+++ force-app/main/default/lwc/parentToChild/parentToChild.js
@@ -2,7 +2,7 @@ import { LightningElement } from 'lwc';
 
 export default class ParentToChild extends LightningElement {
 
-    Candidates = [
+    candidates = [
         { Name: "Varun", CID: 21009 },
         { Name: "Karun", CID: 210010 },
         { Name: "Sharan", CID: 210011 },
@@ -11,16 +11,16 @@ export default class ParentToChild extends LightningElement {
         { Name: "Varen", CID: 210083 }
     ];
 
-    CandidateId;
-    Checked = false;
+    candidateId;
+    checked = false;
 
     processCheckbox(event) {
-        this.Checked = event.target.checked;
+        this.checked = event.target.checked;
     }
 
     tileClickHandler(event) {
-        this.CandidateId = event.detail.CID;
-        this.CandidateName=event.detail.Name;
+        this.candidateId = event.detail.CID;
+        this.candidateName=event.detail.Name;
         console.log(this.CandidateId);
     }
 }
