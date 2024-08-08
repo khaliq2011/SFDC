e4e2e3640a90fb1a9e6674e81d84af469595587d
Vamshi <krishnavamshi6199@gmail.com>
2024-08-09 03:22:35 +0530

    addRemoveRowsDynamically

--- force-app/main/default/lwc/addRemoveRowsDynamically/addRemoveRowsDynamically.js
+++ force-app/main/default/lwc/addRemoveRowsDynamically/addRemoveRowsDynamically.js
@@ -1,62 +1,69 @@
 import { LightningElement, track } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-
 export default class AddRemoveRowsDynamically extends LightningElement {
-    @track ListOfItems = [
-
-        {
-            id: 0
-        }
-
-
-    ]
+    @track ListOfItems = [{ id: 0 }];
+    @track createdRecords = [];  // To store created records
     keyindex = 0;
+    showModal=false;
 
     addRow() {
-        console.log('addRow');
         this.keyindex++;
         this.ListOfItems.push({ id: this.keyindex });
-        console.log(this.ListOfItems);
-
     }
 
     deleteRow(event) {
-        console.log(parseInt(event.target.dataset.id));
-        if (this.ListOfItems.length > 1)
+        if (this.ListOfItems.length > 1) {
             this.ListOfItems = this.ListOfItems.filter(element => element.id !== parseInt(event.target.dataset.id));
+        }
     }
 
     handleSave() {
-        const isValid = true;
+        let isValid = true;
+
         this.template.querySelectorAll('lightning-input-field').forEach(element => {
-            if (!element.reportValidity())
+            if (!element.reportValidity()) {
                 isValid = false;
+            }
         });
 
         if (isValid) {
-            this.template.querySelectorAll('lightning-record-edit-form').forEach(element => element.submit()
-            );
-            this.showToast();
+            this.template.querySelectorAll('lightning-record-edit-form').forEach(element => {
+                element.submit();
+            });
         }
-        console.log(
-            '><><><</></>error');
     }
+
+    handleSuccess(event) {
+        const recordId = event.detail.id;
+        const fields = event.detail.fields;
+
+        this.showToast();
+
+        // Store the created record's information
+        this.createdRecords.push({
+            id: recordId,
+            name: fields.Name.value,
+            Email__c: fields.Email__c.value,
+            Phone__c: fields.Phone__c.value,
+            Email_Opt_Out__c: fields.Email_Opt_Out__c.value
+        });
+    }
+
     showToast() {
+        this.showModal=!this.showModal;
         const event = new ShowToastEvent({
             title: 'Successfully Created',
-            message:
-                'Contact Record has been created',
-            variant: 'success'
+            message: 'Contact Record has been created',
+            variant: 'success',
+            mode: 'pester'
         });
         this.dispatchEvent(event);
     }
+
     resetValues() {
         this.template.querySelectorAll('lightning-input-field').forEach(element => {
-            if (element.value == 'checked')
-                element.value = unchecked;
             element.value = null;
-        }
-        )
+        });
     }
 }
